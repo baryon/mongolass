71fdaafa5f308db2ad6e5aaac36fb23a5cbee83c
lilong <lilong@gmail.com>
2018-06-19 15:52:10 +0900

    测试gridfs

--- test/gridfs.js
+++ test/gridfs.js
@@ -1,29 +1,25 @@
-'use strict';
+const MONGODB = process.env.MONGODB || 'mongodb://localhost:27017/test'
 
-const MONGODB = process.env.MONGODB || 'mongodb://localhost:27017/test';
-
-const assert = require('assert');
-const Mongolass = require('..');
-const mongolass = new Mongolass(MONGODB);
+const assert = require('assert')
+const Mongolass = require('..')
+const mongolass = new Mongolass(MONGODB)
 const fs = require('fs');
-
 
 describe('gridfs.js', function () {
   before(function*() {
   });
 
-  after(function*() {
-    // yield mongolass.gridfs('Test').drop();
-    // mongolass.disconnect();
-  });
+  after(function * () {
+    yield mongolass.disconnect()
+  })
 
   it('uploadStream', function () {
-    var gridfs = mongolass.gridfs('Test');
-    var readStream = fs.createReadStream('../LICENSE');
+    const gridfs = mongolass.gridfs('Test');
+    const readStream = fs.createReadStream('./LICENSE');
     gridfs.openUploadStream('test.dat')
       .then(uploadStream => {
-        var license = fs.readFileSync('./LICENSE');
-        var id = uploadStream.id;
+        const license = fs.readFileSync('./LICENSE');
+        const id = uploadStream.id;
         readStream.pipe(uploadStream);
 
       })
@@ -31,12 +27,12 @@ describe('gridfs.js', function () {
   });
 
   it('uploadFile', function () {
-    var gridfs = mongolass.gridfs('Test');
-    var readStream = fs.createReadStream('../LICENSE');
+    const gridfs = mongolass.gridfs('Test');
+    const readStream = fs.createReadStream('./LICENSE');
     gridfs.openUploadStream('test.dat')
       .then(uploadStream => {
-        var license = fs.readFileSync('./LICENSE');
-        var id = uploadStream.id;
+        const license = fs.readFileSync('./LICENSE');
+        const id = uploadStream.id;
 
         console.log(license)
         uploadStream.write(license);
